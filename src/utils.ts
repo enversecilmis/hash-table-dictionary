84c025f64ef4d42b5d3701534a56b789ff42ae5e
Enver <352563@ogr.ktu.edu.tr>
2022-05-22 15:08:26 +0300

    added fibonacci function

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,19 @@ const searchWordInDisctionary = (searchWord, dictionary) => {
 
 
 
+const fib = (n: number): number => {
+    const table = [1,1]
+
+    for (let i=2; i<n; i++)
+        table[i] = table[i-1] + table[i-2]
+
+    return table[n-1]
+}
+
+
+
 
 export {
     searchWordInDisctionary,
+    fib,
 }
