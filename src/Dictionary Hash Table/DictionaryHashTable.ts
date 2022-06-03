0e3bfea90708835ed85587e69dcccdddb8c9abf4
Enver <352563@ogr.ktu.edu.tr>
2022-06-03 19:27:26 +0300

    factories instead of constructorr + load from file

--- src/Dictionary Hash Table/DictionaryHashTable.ts
+++ src/Dictionary Hash Table/DictionaryHashTable.ts
@@ -1,4 +1,4 @@
-import { createWriteStream, writeFileSync } from "fs"
+import { createWriteStream, readFileSync, writeFileSync } from "fs"
 import { Dictionary, HashStringFunction, StringHashTable, OnCollisionNextIndexHandler, DictionaryHashTableOptions } from "./types"
 import { showStats, simpleNextHash, simpleStringHashFunction } from "./utils"
 
@@ -14,26 +14,7 @@ class DictionaryHashTable {
 
     private allCollisions: number[] = []
 
-
-
-    constructor(
-        dictionary: Dictionary,
-        tableSize: number,
-        options: DictionaryHashTableOptions = { hashFunction: simpleStringHashFunction, nextHash: simpleNextHash }
-    ){
-        if(tableSize <= dictionary.length) throw new Error('Hash table size must be bigger than dictionary size.')
-        this.hashFunc = options.hashFunction
-        this.nextHash = options.nextHash
-        this.hashTable = Array(tableSize)
-        
-        // Fill in the hash table.
-        dictionary.forEach( pair => this.add(pair))
-        
-
-        // Statistics:
-        console.log("*** Hash table created ***")
-        showStats(this.allCollisions)
-    }
+    constructor(){}
 
 
 
@@ -85,6 +66,57 @@ class DictionaryHashTable {
             row ?
               logger.write( `${row[0]}  ${row[1]} \r\n`):
               logger.write("\r\n")
+    }
+
+
+
+
+    static loadFromFile(path: string){
+        const file = readFileSync(path, { encoding: 'utf-8' })
+
+        // Split file line by line.
+        const lines = file.split("\r\n")
+
+        // From each line, get word and its translation as tuple, which are seperated by two spaces.
+        // Put undefined in place of empty line.
+        const dictionary: Dictionary = lines.map( line => {
+            const pair = line.split(/ {2,}/)
+            return pair[0]?
+                    [pair[0], pair[1]]:
+                    undefined
+        })
+
+        return dictionary
+    }
+
+
+
+
+    static createFromDictionary(
+        dictionary: Dictionary,
+        tableSize: number,
+        options: DictionaryHashTableOptions = { showStats: false }
+    ): DictionaryHashTable {
+        if(tableSize <= dictionary.length) throw new Error('Hash table size must be bigger than dictionary size.')
+        
+        const dictionaryHashTable     = new DictionaryHashTable()
+        dictionaryHashTable.hashFunc  = options.hashFunction || simpleStringHashFunction
+        dictionaryHashTable.nextHash  = options.nextHash || simpleNextHash
+        dictionaryHashTable.hashTable = Array(tableSize)
+        
+        // Fill in the hash table.
+        dictionary.forEach( pair => dictionaryHashTable.add(pair))
+        
+        // Statistics:
+        if(options.showStats){
+            console.log("*** Hash table created ***")
+            console.log("Collision Stats:");
+            
+            showStats(dictionaryHashTable.allCollisions)
+        }
+        delete dictionaryHashTable.allCollisions  // So that it won't take up space in memory
+        
+        return dictionaryHashTable
     }
     
 }
